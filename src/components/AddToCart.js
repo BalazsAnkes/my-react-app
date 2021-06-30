b93ca4994d2203605d67475bfbc090bcbf8eaf43
Balazs Ankes <balazs.ankes@gmail.com>
2021-06-30 10:45:18 +0200

    feat(AddToCart): show success message

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,19 +1,27 @@
 import { useTranslation } from "react-i18next";
 import PropTypes from 'prop-types';
 import { wait } from '../utils/index';
+import { useState } from "react";
 
 const AddToCart = ({ state, onAddToCart }) => {
+    const [isSuccessShown, setIsSuccessShown] = useState(false);
     const { t } = useTranslation();
     const onClick = async () => {
         onAddToCart({ label: 'button.loading', disabled: true });
         await wait(2000);
         onAddToCart({ label: 'button.viewCart', disabled: false });
+        setIsSuccessShown(true);
     };
 
     return (
-        <button disabled={state.disabled} onClick={onClick} className="add-to-cart__btn btn btn--borderless btn--primary">
-            {t(state.label)}
-        </button>
+        <>
+            <button disabled={state.disabled} onClick={onClick} className="add-to-cart__btn btn btn--borderless btn--primary">
+                {t(state.label)}
+            </button>
+            {isSuccessShown && <p className="success-msg">
+                {t('itemAdded')}
+            </p>}
+        </>
     );
 };
 
